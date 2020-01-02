23dc0a254ddb84812eede1f08afd5bb135ffc8eb
Jianguo Ma <jiangma@microsoft.com>
2020-01-02 15:12:03 +0800

    Restart node agent after the node is added to static cluster

--- add-nodes/src/main/scripts/add-to-cluster.py
+++ add-nodes/src/main/scripts/add-to-cluster.py
@@ -6,3 +6,7 @@ node = AdminConfig.getid('/Node:${NODE_NAME}/')
 AdminConfig.createClusterMember(cluster, node, [['memberName', '${CLUSTER_MEMBER_NAME}']])
 AdminConfig.save()
 AdminNodeManagement.syncActiveNodes()
+
+# Restart node agent
+na = AdminControl.queryNames('type=NodeAgent,node=${NODE_NAME},*')
+AdminControl.invoke(na, 'restart', 'true true')
