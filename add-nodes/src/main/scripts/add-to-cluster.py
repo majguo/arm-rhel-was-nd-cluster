3465f5b197933d50f48bffa0844d14a142c02718
Jianguo Ma <jiangma@microsoft.com>
2020-01-02 20:56:15 +0800

    start cluster member before restarting node agent

--- add-nodes/src/main/scripts/add-to-cluster.py
+++ add-nodes/src/main/scripts/add-to-cluster.py
@@ -7,6 +7,9 @@ AdminConfig.createClusterMember(cluster, node, [['memberName', '${CLUSTER_MEMBER
 AdminConfig.save()
 AdminNodeManagement.syncActiveNodes()
 
+# Start server
+AdminControl.startServer('${CLUSTER_MEMBER_NAME}', '${NODE_NAME}')
+
 # Restart node agent
 na = AdminControl.queryNames('type=NodeAgent,node=${NODE_NAME},*')
 AdminControl.invoke(na, 'restart', 'true true')
--- add-nodes/src/main/scripts/install.sh
+++ add-nodes/src/main/scripts/install.sh
@@ -71,9 +71,6 @@ add_to_cluster() {
     sed -i "s/\${CLUSTER_NAME}/${clusterName}/g" add-to-cluster.py
     sed -i "s/\${CLUSTER_MEMBER_NAME}/${clusterMemberName}/g" add-to-cluster.py
     /opt/IBM/WebSphere/ND/V9/profiles/${profileName}/bin/wsadmin.sh -lang jython -f add-to-cluster.py
-   
-    # Start cluster member server
-    /opt/IBM/WebSphere/ND/V9/profiles/${profileName}/bin/startServer.sh ${clusterMemberName}
     echo "Node ${nodeName} is successfully added to cluster ${clusterName}"
 }
 
