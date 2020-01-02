eb3d121053198eb07671d7c65b593c9a7ca02a3a
Jianguo Ma <jiangma@microsoft.com>
2020-01-02 23:19:06 +0800

    test

--- add-nodes/src/main/scripts/add-to-cluster.py
+++ add-nodes/src/main/scripts/add-to-cluster.py
@@ -10,6 +10,8 @@ AdminNodeManagement.syncActiveNodes()
 # Start server
 AdminControl.startServer('${CLUSTER_MEMBER_NAME}', '${NODE_NAME}')
 
-# Restart node agent
+# Restart node agent & server
 na = AdminControl.queryNames('type=NodeAgent,node=${NODE_NAME},*')
 AdminControl.invoke(na, 'restart', 'true true')
+AdminControl.stopServer('${CLUSTER_MEMBER_NAME}', '${NODE_NAME}')
+AdminControl.startServer('${CLUSTER_MEMBER_NAME}', '${NODE_NAME}')
