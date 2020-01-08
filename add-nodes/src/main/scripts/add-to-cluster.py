0716d07ee3bb99a6865148a8cdd0becad5570d13
Jianguo Ma <jiangma@microsoft.com>
2020-01-08 23:08:45 +0800

    Immediately save once the node is being added as cluster member

--- add-nodes/src/main/scripts/add-to-cluster.py
+++ add-nodes/src/main/scripts/add-to-cluster.py
@@ -4,8 +4,12 @@ cluster = AdminConfig.getid('/ServerCluster:${CLUSTER_NAME}/')
 # Add node as cluster member
 node = AdminConfig.getid('/Node:${NODE_NAME}/')
 AdminConfig.createClusterMember(cluster, node, [['memberName', '${CLUSTER_MEMBER_NAME}']])
+AdminConfig.save()
+
+# Modify nodeRestartState of cluster member as PREVIOUS
 server = AdminConfig.getid('/Server:${CLUSTER_MEMBER_NAME}/')
 mp = AdminConfig.list('MonitoringPolicy', server)
 AdminConfig.modify(mp, '[[nodeRestartState PREVIOUS]]')
 AdminConfig.save()
+
 AdminNodeManagement.syncActiveNodes()
